9b32d6172179e5aca9c3789153ae26e5061fce27
ma-cru <101591711+ma-cru@users.noreply.github.com>
2022-04-09 17:18:33 +0200

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,7 +8,7 @@ const Menu = props => {
             <form className="listSection" onSubmit={props.submitHandler}>
                 <span>❖</span>
                 <label><strong>{props.product}</strong><br />{props.description}<br /><em>€{props.price}</em></label>
-                <input id={props.id} name={props.product} defaultValue="0" className="sectionInputs" type="number" /*value={initAge}*/ min="0" onBlur={props.valueTallier}></input> <button className="sectionButtons" value={props.price} type="submit">Order</button>
+                <input id={props.id} name={props.product} defaultValue="0" className="sectionInputs" type="number" min="0" onBlur={props.valueTallier}></input> <button className="sectionButtons" value={props.price} type="submit">Order</button>
             </form>
             <hr />
         </React.Fragment>
