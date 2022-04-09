85b8e11db9ddd633d224ab9ce4b7f176c25225ad
ma-cru <101591711+ma-cru@users.noreply.github.com>
2022-04-09 17:13:36 +0200

    Update Einkaufswagen.js

--- src/overlays/Einkaufswagen.js
+++ src/overlays/Einkaufswagen.js
@@ -10,14 +10,12 @@ const ModalOverlay = props => {
     return (
         <Fragment>
             <div id="modalMsg" className={`modal ${props.classesProp}`}>
-                <div className="modalContent" /*style={{height: props.responsiveHeight}}*/>
+                <div className="modalContent">
                     <div className="modalBanner"><span className="close" onClick={props.modalToggler}>&times;</span><br /><span className='buttonTitle'>Wilkommen im Einkaufswagen</span></div>
                     <div className="listeContainer">
                         <p>Please finalise and submit your order.</p>
                         <CartElement
                             modalToggler={props.modalToggler}
-                            /*clearOrder={props.clearOrder}*/
-                            /*amountTotalerTrigger={props.amountTotalerTrigger}*/
                         />
                     </div>
                 </div>
@@ -33,12 +31,9 @@ const Einkaufswagen = (props) => {
         <Fragment>
             {ReactDOM.createPortal(
                 <ModalOverlay
-                    /*responsiveHeight={props.responsiveHeight}*/
                     classesProp={props.classesProp}
                     modalToggler={props.modalToggler}
                     modalMsg={props.modalMsg}
-                    /*clearOrder={props.clearOrder}*/
-                    /*amountTotalerTrigger={props.amountTotalerTrigger}*/
                 />,
                 document.getElementById('overlay-root')
             )}
