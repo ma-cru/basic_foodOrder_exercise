1476b30fd3ab580b46a221757e8a7a0405555ee8
ma-cru <101591711+ma-cru@users.noreply.github.com>
2022-04-09 17:06:59 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import Hintergrund from './components/Hintergrund';
+
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+      <Hintergrund/>
+    </React.Fragment>
+  );
+}
+
+export default App;
